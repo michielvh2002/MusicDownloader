5cdc7cc95e02358a04910163cb991b30c90c10c5
michiel.van.himbeeck@outlook.com <michiel.van.himbeeck@outlook.com>
2022-06-23 22:23:01 +0200

    Joe Biden

---
+++ MDApp/Videohandler.cs
@@ -0,0 +1,70 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Converter;
+
+namespace MDApp
+{
+    public class Videohandler
+    {
+        private YoutubeClient youtube = new();
+        public async Task<Video> GetMetaData(string url)
+        {
+            return await youtube.Videos.GetAsync(url);
+        }
+
+        public async Task<bool> DownloadAsMp4(string url)
+        {
+            var vid = await GetMetaData(url);
+            return await DownloadMp4(vid);
+        }
+        private async Task<bool> DownloadMp4(Video vid)
+        {
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(vid.Id);
+
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{vid.Title}.{streamInfo.Container}");
+            return true;
+        }
+
+        public async Task<bool> DownloadAsMp3(string url)
+        {
+            Video vid = await GetMetaData(url);
+            StreamManifest streamManifest = await youtube.Videos.Streams.GetManifestAsync(vid.Id);
+
+            IStreamInfo streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+            
+            Console.WriteLine(streamInfo.Container);
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{vid.Title}.{streamInfo.Container}");
+            return true;
+        }
+
+        public async Task<bool> DownloadPlaylist(string url, string type)
+        {
+
+            if (string.IsNullOrWhiteSpace(url))
+                throw new ArgumentException("Url can not be empty");
+            if (!url.Contains("list="))
+                throw new ArgumentException("This is not a playlist");
+            if (!url.Contains("youtube")) throw new ArgumentException("Only yt bro");
+
+            var playList = await youtube.Playlists.GetAsync("PL_9VHR_SV37ZQ4J66LnNp2haPdiyDmuef");
+            var videos = youtube.Playlists.GetVideosAsync(playList.Id);
+
+            await foreach (var video in videos)
+            {
+                switch (type)
+                {
+                    case "mp4": await DownloadAsMp4(video.Url);
+                        break;
+                    case "mp3": await DownloadAsMp3(video.Url);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ MDTests/Program.cs
@@ -0,0 +1,47 @@
+﻿using MDApp;
+namespace MDTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            printAsync();
+            Thread.Sleep(100000);
+        }
+        private static async void printAsync()
+        {
+            Videohandler handler = new();
+            var yes = await handler.GetMetaData("https://www.youtube.com/watch?v=l2hA8g1cNvQ");
+
+            Console.WriteLine(yes.Author);
+            Console.WriteLine(yes.Description);
+            Console.WriteLine(yes.Duration);
+
+            try
+            {
+                bool download = await handler.DownloadAsMp3("https://www.youtube.com/watch?v=l2hA8g1cNvQ");
+                if (download) Console.WriteLine("Succes");
+                else Console.WriteLine("Failed");
+
+                await handler.DownloadPlaylistAsMp4("hhttps://www.youtube.com/watch?v=nCg3ufihKyU&list=PL_9VHR_SV37ZQ4J66LnNp2haPdiyDmuef");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                bool download = await handler.DownloadAsMp4("https://www.youtube.com/watch?v=MF1qEhBSfq4");
+                if (download) Console.WriteLine("Succes");
+                else Console.WriteLine("Failed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
